ddf1a06abaeee3b16c3682b6d4a45756051ebf32
tensor-flower <wanqiguo2@gmail.com>
2019-04-01 15:53:10 +0800

    a

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const card = ({id,name,email})=>{
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='placeholder' 
-            src={`https://robohash.org/${id}?200x200`}></img>
+            src={`https://robohash.org/${id}?size=200x200`}></img>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
