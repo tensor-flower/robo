b1fc7d428e8210d965774f94e5c686e867591a80
tensor-flower <wanqiguo2@gmail.com>
2019-04-01 14:57:47 +0800

    Updates

---
+++ precache-manifest.c1caad1dc7d017faaf99fa60a9a8274c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "abc8f461107e98c41f89",
+    "url": "/static/js/main.d40394f2.chunk.js"
+  },
+  {
+    "revision": "4a6cc83a3a731033777f",
+    "url": "/static/js/2.d0217807.chunk.js"
+  },
+  {
+    "revision": "abc8f461107e98c41f89",
+    "url": "/static/css/main.855b729a.chunk.css"
+  },
+  {
+    "revision": "4a6cc83a3a731033777f",
+    "url": "/static/css/2.d7f4aa86.chunk.css"
+  },
+  {
+    "revision": "71880b1fbd44531290fe29e668b603d7",
+    "url": "/index.html"
+  }
+];
