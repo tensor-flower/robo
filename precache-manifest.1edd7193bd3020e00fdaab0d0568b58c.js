4e11fbe347264efcf8bc6c07f045fef97f59900c
tensor-flower <wanqiguo2@gmail.com>
2019-04-01 14:59:54 +0800

    Updates

---
+++ precache-manifest.1edd7193bd3020e00fdaab0d0568b58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0e91b9d4374e11fdf2b",
+    "url": "/robo/static/js/runtime~main.ff88e54f.js"
+  },
+  {
+    "revision": "abe8b210308019546ecf",
+    "url": "/robo/static/js/main.56b2b932.chunk.js"
+  },
+  {
+    "revision": "4a6cc83a3a731033777f",
+    "url": "/robo/static/js/2.d0217807.chunk.js"
+  },
+  {
+    "revision": "abe8b210308019546ecf",
+    "url": "/robo/static/css/main.855b729a.chunk.css"
+  },
+  {
+    "revision": "4a6cc83a3a731033777f",
+    "url": "/robo/static/css/2.d7f4aa86.chunk.css"
+  },
+  {
+    "revision": "8d93159203986af83788f37db8f69c82",
+    "url": "/robo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1caad1dc7d017faaf99fa60a9a8274c.js"
+  "/robo/precache-manifest.1edd7193bd3020e00fdaab0d0568b58c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/robo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
